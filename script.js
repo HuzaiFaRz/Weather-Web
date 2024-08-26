51572ff54ee604ae78965fd0f711543a2a9915f4
HuzaiFa A <167712059+HuzaiFaRz@users.noreply.github.com>
2024-08-26 11:36:47 +0500

    Some

--- script.js
+++ script.js
@@ -85,6 +85,7 @@
   const errorContainer = document.querySelector(".error-container");
   const errorDiv = document.querySelector(".error");
   const errorCloseBtn = document.querySelector(".error-close");
+
   const tempDiv = document.querySelector(".temp");
   const tempFeelLikeDiv = document.querySelector(".feel-like-div");
   const tempMaxDiv = document.querySelector(".temp-max-div");
@@ -114,10 +115,9 @@
   };
 
   const weatherMain = async () => {
-    let searchInputValueGetting = new FormData(form)
-      .get("search")
-      .trim()
-      .toLowerCase();
+    let searchInputValueGetting = new FormData(form).get("search").trimEnd();
+    console.log(searchInputValueGetting);
+
     if (!searchInputValueGetting) {
       errorContainerVisible();
       errorCloseBtn.addEventListener("click", errorContainerUnVisible);
@@ -127,132 +127,120 @@
     }
     const apiKey = "112643c301e40cb81b9c70b2466c88f8";
     const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${searchInputValueGetting}&appid=${apiKey}`;
+
     try {
       const weatherURLFetching = await fetch(apiURL);
-      const weatherURLFetchedData = weatherURLFetching
-        .json()
-        .then((data) => {
-          let { main, wind, weather, clouds, sys, dt, name } = data;
-
-          const searchCountryFullName = new Intl.DisplayNames([sys.country], {
-            type: "region",
-          }).of(sys.country);
-
-          const apiTime = new Date(dt * 1000);
-          const fullDate = {
-            hour: "numeric",
-            minute: "2-digit",
-            day: "numeric",
-            weekday: "long",
-            month: "long",
-            year: "numeric",
-          };
-          let convertedTime = new Intl.DateTimeFormat("en-US", fullDate).format(
-            apiTime
-          );
-
-          let tempConverted = Number(
-            main.temp.toFixed().toString().slice(0, -1)
-          );
-
-          let feelLikeTempConverted = Number(
-            main.feels_like.toFixed().toString().slice(0, -1)
-          );
-          let minTempConverted = Number(
-            main.temp_min.toFixed().toString().slice(0, -1)
-          );
-          let maxTempConverted = Number(
-            main.temp_max.toFixed().toString().slice(0, -1)
-          );
-          let weatherConditionLongInfoConverted =
-            weather[0].description.charAt().toUpperCase() +
-            weather[0].description.slice(1, weather[0].description.lenght);
-
-          tempDiv.innerHTML = `${tempConverted}&#176`;
-          nameDiv.innerHTML = `${name},  ${searchCountryFullName}`;
-          dateDiv.innerHTML = convertedTime;
-          weatherConditionShortInfoDiv.innerHTML = `${weather[0].main}`;
-          weatherConditionLongInfoDiv.innerHTML =
-            weatherConditionLongInfoConverted;
-          tempFeelLikeDiv.innerHTML = `${feelLikeTempConverted}&#176 <i class="ri-temp-hot-line feel-like-icon"></i>`;
-          tempMinDiv.innerHTML = `${minTempConverted}&#176 <i class="ri-temp-cold-line temp-min-icon"></i>`;
-          tempMaxDiv.innerHTML = `${maxTempConverted}&#176  <i class="ri-temp-hot-line temp-max-icon"></i>`;
-          humadityDiv.innerHTML = `${main.humidity}% <i class="ri-drop-line humadity-icon"></i>`;
-          cloudyDiv.innerHTML = `${clouds.all}% <i class="ri-cloud-line cloudy-icon"></i>`;
-          windDiv.innerHTML = `${wind.speed.toFixed()} km/h <i class="ri-windy-line wind-icon"></i>`;
-          pressureDiv.innerHTML = `${main.pressure}hPa <i class="ri-windy-line pressure-icon"></i>`;
-
-          if (convertedTime.includes("PM")) {
-            if (weatherConditionShortInfoDiv.innerHTML === "Thunderstorm") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[0].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Haze") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[1].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Clouds") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[2].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Clear") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[3].bgURL})`;
-            } else if (
-              weatherConditionShortInfoDiv.innerHTML === "Rain" ||
-              weatherConditionShortInfoDiv.innerHTML === "Drizzle"
-            ) {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[4].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Sunny") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[5].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Smoke") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[6].bgURL})`;
-            }
-          }
-          if (convertedTime.includes("AM")) {
-            if (weatherConditionShortInfoDiv.innerHTML === "Thunderstorm") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[0].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Haze") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[1].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Clouds") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[2].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Clear") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[3].bgURL})`;
-            } else if (
-              weatherConditionShortInfoDiv.innerHTML === "Rain" ||
-              weatherConditionShortInfoDiv.innerHTML === "Drizzle"
-            ) {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[4].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Sunny") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[5].bgURL})`;
-            } else if (weatherConditionShortInfoDiv.innerHTML === "Smoke") {
-              weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[6].bgURL})`;
-            }
-          }
-        })
-        .catch((underError) => {
-          underError =
-            "It looks like we're having trouble accessing the country information. It might be missing or not set up correctly. Please check the data source or ensure the information is provided.";
-          errorContainerVisible();
-          errorCloseBtn.addEventListener("click", errorContainerUnVisible);
-          searchInput.value = "";
-          errorDiv.innerHTML = underError;
-        })
-        .finally((execution = "Complete") => {
-          console.log(execution);
-          searchInput.value = "";
-        });
-
+      const weatherURLFetchedData = await weatherURLFetching.json();
       if (!weatherURLFetching.ok) {
         errorContainerVisible();
         errorCloseBtn.addEventListener("click", errorContainerUnVisible);
         searchInput.value = "";
-        errorDiv.innerHTML =
-          "Network response was not ok. </br> Wrong Spelling.";
+        errorDiv.innerHTML = `${weatherURLFetchedData.cod}</br>${weatherURLFetchedData.message}`;
+      }
+      console.log(weatherURLFetchedData);
+
+      let { main, wind, weather, clouds, sys, dt, name } =
+        weatherURLFetchedData;
+
+      const searchCountryFullName = new Intl.DisplayNames([sys.country], {
+        type: "region",
+      }).of(sys.country);
+
+      const apiTime = new Date(dt * 1000);
+      const fullDateOption = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        timeZoneName: "short",
+      };
+      let convertedTime = new Intl.DateTimeFormat(
+        "en-US",
+        fullDateOption
+      ).format(apiTime);
+
+      let weatherConditionLongInfoConverted =
+        weather[0].description.charAt().toUpperCase() +
+        weather[0].description.slice(1, weather[0].description.lenght);
+
+      let tempConverted = (main.temp - 273.15).toFixed();
+
+      let feelLikeTempConverted = (main.feels_like - 273.15).toFixed(1);
+      let minTempConverted = (main.temp_min - 273.15).toFixed(1);
+      let maxTempConverted = (main.temp_max - 273.15).toFixed(1);
+
+      tempDiv.innerHTML = `${tempConverted}&#176`;
+      tempFeelLikeDiv.innerHTML = `${feelLikeTempConverted}&#176 <i class="ri-temp-hot-line feel-like-icon"></i>`;
+      tempMinDiv.innerHTML = `${minTempConverted}&#176 <i class="ri-temp-cold-line temp-min-icon"></i>`;
+      tempMaxDiv.innerHTML = `${maxTempConverted}&#176  <i class="ri-temp-hot-line temp-max-icon"></i>`;
+
+      nameDiv.innerHTML = `${name},  ${searchCountryFullName}`;
+      dateDiv.innerHTML = convertedTime;
+      weatherConditionShortInfoDiv.innerHTML = `${weather[0].main}`;
+      weatherConditionLongInfoDiv.innerHTML = weatherConditionLongInfoConverted;
+      weatherConditionIcon.setAttribute(
+        "src",
+        `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`
+      );
+
+      humadityDiv.innerHTML = `${main.humidity}% <i class="ri-drop-line humadity-icon"></i>`;
+      cloudyDiv.innerHTML = `${clouds.all}% <i class="ri-cloud-line cloudy-icon"></i>`;
+      windDiv.innerHTML = `${wind.speed.toFixed()} km/h <i class="ri-windy-line wind-icon"></i>`;
+      pressureDiv.innerHTML = `${main.pressure} hPa <i class="ri-windy-line pressure-icon"></i>`;
+
+      if (convertedTime.includes("PM")) {
+        if (weatherConditionShortInfoDiv.innerHTML === "Thunderstorm") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[0].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Haze") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[1].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Clouds") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[2].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Clear") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[3].bgURL})`;
+        } else if (
+          weatherConditionShortInfoDiv.innerHTML === "Rain" ||
+          weatherConditionShortInfoDiv.innerHTML === "Drizzle"
+        ) {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[4].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Sunny") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[5].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Smoke") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesPMURL[6].bgURL})`;
+        }
+      } else {
+        if (weatherConditionShortInfoDiv.innerHTML === "Thunderstorm") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[0].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Haze") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[1].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Clouds") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[2].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Clear") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[3].bgURL})`;
+        } else if (
+          weatherConditionShortInfoDiv.innerHTML === "Rain" ||
+          weatherConditionShortInfoDiv.innerHTML === "Drizzle"
+        ) {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[4].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Sunny") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[5].bgURL})`;
+        } else if (weatherConditionShortInfoDiv.innerHTML === "Smoke") {
+          weatherContainer.style.backgroundImage = `url(${backgroundImagesAMURL[6].bgURL})`;
+        }
       }
     } catch (mainError) {
+      // mainError = `${weatherURLFetchedData.message}`;
       mainError =
-        "It looks like we're having trouble accessing the country information. It might be missing or not set up correctly. Please check the data source or ensure the information is provided.";
+        "An error occurred because some information is missing or there is a spelling mistake. Please check and fill out all required fields correctly.";
       errorContainerVisible();
       errorCloseBtn.addEventListener("click", errorContainerUnVisible);
       searchInput.value = "";
       errorDiv.innerHTML = mainError;
+    } finally {
+      searchInput.value = "";
     }
   };
-
   weatherMain();
 
   form.addEventListener("submit", (event) => {
@@ -260,3 +248,5 @@
     weatherMain();
   });
 })();
+
+console.log(305 - 273.15);
